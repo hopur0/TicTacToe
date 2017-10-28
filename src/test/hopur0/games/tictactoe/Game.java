aa8f5f0dd4e47b83cd464927aeea59e5f334ccc3
Viktor Sævarsson <viktor.saevars@gmail.com>
2017-10-28 17:00:03 +0000

    Travis integration (#1)
    * added .travis.yml
    
    * added test test

---
+++ src/test/hopur0/games/tictactoe/Game.java
@@ -0,0 +1,12 @@
+package hopur0.games.tictactoe;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+import org.junit.Test;
+
+public class Game {
+    @Test
+    public void testGame() {
+        assertEquals(1, 1);
+    }
+}
