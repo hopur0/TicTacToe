10c970a80fe568ddd3d6c2d3d0f6ca4ebb3be917
Daði Sigursveinn Harðarson <dadi.sigursveinn@gmail.com>
2017-10-28 20:23:29 +0000

    created the UI interface (#8)
    * created the UI interface
    
    * updated function names
    
    * Changed Character to char

---
+++ src/main/hopur0/games/tictactoe/UI.java
@@ -0,0 +1,8 @@
+package hopur0.games.tictactoe;
+
+public abstract class UI {
+	public abstract int options(String[] options);
+	public abstract String getString(String message);
+	public abstract char getCharacter(String message);
+	public abstract int getInt(String message);
+}
