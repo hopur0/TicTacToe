1f39aa320fd30e8545af23499b983f7f68bce418
Fanarito <viktor.saevars@gmail.com>
2017-10-28 19:14:20 +0000

    added eclipse support

--- src/test/hopur0/games/tictactoe/GameTest.java
+++ src/test/hopur0/games/tictactoe/GameTest.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 import org.junit.Test;
 
-public class Game {
+public class GameTest {
     @Test
     public void testGame() {
         assertEquals(1, 1);
