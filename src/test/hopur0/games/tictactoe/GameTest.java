b10dcfa5ec46957470cd025d1529a1e9012f232d
hrafnlogi <hrafnhe230@nemi.tskoli.is>
2017-10-29 15:16:59 +0000

    Gametest branch (#22)
    * Test Initialize Players
    
    * added test for initializing players
    
    * Test Initialize Players

--- src/test/hopur0/games/tictactoe/GameTest.java
+++ src/test/hopur0/games/tictactoe/GameTest.java
@@ -2,7 +2,23 @@ package hopur0.games.tictactoe;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
+
+import java.io.ByteArrayInputStream;
+
 import org.junit.Test;
 
 public class GameTest {
+	@Test
+    public void testInitializingPlayers() {
+		
+		String data = "Viktor\nHrafn\n1\n4\n2\n5\n3\n";
+		Game g = new Game(new ByteArrayInputStream(data.getBytes()));
+			
+		Player player0 = new Player("Viktor", 'X');
+		Player player1 = new Player("Hrafn", 'O');
+			
+		assertEquals(g.players[0].getName(), "Viktor");
+		assertEquals(g.players[1].getName(), "Hrafn");
+		
+    }
 }
