23eb355b698c4b3e6a1e666aff1b68b7f4458ed7
hrafnlogi <krummi3@gmail.com>
2017-10-29 14:39:59 +0000

    Test Initialize Players

--- src/test/hopur0/games/tictactoe/GameTest.java
+++ src/test/hopur0/games/tictactoe/GameTest.java
@@ -5,4 +5,13 @@ import static org.junit.Assert.fail;
 import org.junit.Test;
 
 public class GameTest {
+	@Test
+    public void testInitializingPlayers() {
+		UI ui = new TUI();
+		Game g = new Game(ui);		
+		
+		assertEquals(g.players[0], 'X');
+		assertEquals(g.players[1], 'O');	
+		
+    }
 }
