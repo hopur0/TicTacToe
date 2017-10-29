2fe7037d325e04841159096375d35185bb64d85b
Fanarito <viktor.saevars@gmail.com>
2017-10-29 00:27:18 +0000

    implemented the most basic game loop possible

--- src/main/hopur0/games/tictactoe/Game.java
+++ src/main/hopur0/games/tictactoe/Game.java
@@ -1,7 +1,44 @@
 package hopur0.games.tictactoe;
 
 class Game {
-    public static void main(String[] args) {
-        System.out.println("what a man is Salah");
+  UI ui;
+  Board board;
+  Player[] players;
+
+  public Game(UI ui) {
+    this.ui = ui;
+    board = new Board();
+    initializePlayers();
+  }
+
+  private void initializePlayers() {
+    String firstPlayerName = ui.getString("Enter first player name:");
+    String secondPlayerName = ui.getString("Enter second player name:");
+    players = new Player[] {
+      new Player(firstPlayerName, 'x'),
+      new Player(secondPlayerName, 'o')
+    };
+  }
+
+  private void gameLoop() {
+    int currentPlayer = 0;
+
+    while (board.winner() == null && !board.full()) {
+      ui.showMessage("Current state:" + System.lineSeparator() + board.toString());
+      ui.showMessage(players[currentPlayer].getName() + " plays:");
+      int selectedTile = ui.getInt("Enter tile number:");
+      board.setTile(players[currentPlayer], selectedTile);
+      currentPlayer = (currentPlayer + 1) % 2;
     }
+
+    if (board.full()) {
+      ui.showMessage("Draw!");
+    } else {
+      Player winner = board.winner();
+      ui.showMessage(winner.getName() + " is the winner!");
+    }
+  }
+
+  public static void main(String[] args) {
+  }
 }
