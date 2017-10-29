ad4a38aad6153132cd9b402ec5149f9281f8e90a
Fanarito <viktor.saevars@gmail.com>
2017-10-29 16:32:24 +0000

    remove uneccessary constructors

--- src/test/hopur0/games/tictactoe/GameTest.java
+++ src/test/hopur0/games/tictactoe/GameTest.java
@@ -13,8 +13,6 @@ public class GameTest {
     public void testInitializingPlayers() {
 		String data = "Viktor\nHrafn\n1\n4\n2\n5\n3\n3\n";
 		Game g = new Game(new ByteArrayInputStream(data.getBytes()));
-		Player player0 = new Player("Viktor", 'X');
-		Player player1 = new Player("Hrafn", 'O');
 		assertEquals(g.players[0].getName(), "Viktor");
 		assertEquals(g.players[1].getName(), "Hrafn");
     }
